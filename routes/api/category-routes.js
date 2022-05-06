63e406ecb5e08c9b684db623bebb910085d98222
schaparro08 <101684009+schaparro08@users.noreply.github.com>
2022-05-05 22:46:38 -0400

    addition of category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,31 +3,95 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll({
+      include: [products]
+    });
+    res.status(200).json(categoryData);
+    // using first function from mini proj
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
-  Category.findAll({
-    include: [products],
-  })
-  .then((categories ) => res.json(categories))
-  .catch((err) => res.status(500).json(err));
+  // Category.findAll({
+  //   include: [products],
+  // })
+  // .then((categories ) => res.json(categories))
+  // .catch((err) => res.status(500).json(err));
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Category}]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name,
+      
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updateCategory) => {
+      // Sends the updated book as a json response
+      res.json(updateCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
